a8d7154cff2483c3353f85f0c1e592b8894510f4
EricYuan26 <117863091+EricYuan26@users.noreply.github.com>
2023-06-21 20:21:09 -0400

    lol i was dumb and forgor to include the new tank def in the last push

--- src/Const/TankDefinitions.ts
+++ src/Const/TankDefinitions.ts
@@ -161,7 +161,8 @@ const TankDefinitions = JSON.parse(`[
             6,
             7,
             8,
-            36
+            36,
+            100
         ],
         "flags": {
             "invisibility": false,
